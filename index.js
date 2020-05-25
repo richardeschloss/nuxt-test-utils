2854d926833aac8e38f5a61ad48beb30e9c500a3
Richard Schloss <richard.e.schloss@gmail.com>
2020-05-24 22:02:16 -0700

    Added delay and nextTickP

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const { getModuleOptions, ModuleContext } = require('./dist/moduleUtils')
 const { compilePlugin, injectPlugin, PluginContext } = require('./dist/pluginUtils')
 
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+const nextTickP = (ctx) => {
+  return new Promise((resolve) => {
+    ctx.$nextTick(resolve)
+  })
+}
+
 module.exports = {
+  delay,
+  nextTickP,
   getModuleOptions,
   compilePlugin,
   injectPlugin,
